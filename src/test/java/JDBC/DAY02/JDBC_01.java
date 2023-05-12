754360b9dd5d718fd40945f280a4b1c6a4da8852
Abdulkadir <aatug@overstock.com>
2023-05-11 20:27:43 -0400

    commit 4

--- src/test/java/JDBC/DAY02/JDBC_01.java
+++ src/test/java/JDBC/DAY02/JDBC_01.java
@@ -2,6 +2,7 @@ package JDBC.DAY02;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
@@ -21,7 +22,9 @@ public class JDBC_01 {
     * getObject() -> reads the data from column
     * */
     /*ResultSet Types
-
+      type_scroll_insensitive: the cursor can scroll forward and backward
+      type_scroll_insensitive: the cursor can scroll forward and backward but others can not change your database
+      concur_read_only: read-only. this is the default
     * */
 
     private static Connection connection;
@@ -43,16 +46,90 @@ public class JDBC_01 {
         }
     }
     public static Statement getStatement() throws SQLException {
-        return statement=connection.createStatement();
+        return statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
     }
     public static ResultSet getResultSet(String tableName) throws SQLException {
-        return  resultSet=statement.executeQuery("Select * from"+ tableName);
+        return  resultSet=statement.executeQuery("Select * from "+ tableName);
     }
     @AfterEach
     public void closeDB() throws SQLException {
       connection.close();
       statement.close();
       resultSet.close();
+    }
+
+    @Test
+    public void test1() throws SQLException {
+        getStatement();
+        getResultSet("employees");
+
+        //1st row
+        resultSet.next();
+        System.out.println("Employee ID: " + resultSet.getString(1) + "\n" +
+                "First Name: " + resultSet.getString("first_name") );
+    }
+
+    @Test
+    public void test2() throws SQLException {
+        getStatement();
+        getResultSet("countries");
+
+        //1st row
+        resultSet.next();
+        System.out.println("I'm in First Row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        //2nd row
+        resultSet.next();
+        System.out.println("I'm in Second Row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        //move to first row from second row
+        resultSet.previous();
+        System.out.println("I'm in First Row by using previous method");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        resultSet.next();
+        System.out.println("I'm in Second Row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        resultSet.next();
+        System.out.println("I'm in 3rd Row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        //move cursor to the first row
+        resultSet.first();
+        System.out.println("I'm in " + resultSet.getRow() +"st row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        resultSet.next();
+        System.out.println("I'm in " + resultSet.getRow() +"nd row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        resultSet.beforeFirst();
+        //will go above result table and it'll not read the table
+        /*System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));*/
+
+        resultSet.next();
+        System.out.println("I'm in " + resultSet.getRow() +"st row");
+        System.out.println("Country ID: " + resultSet.getString(1) + "\n" +
+                "Country Name: " + resultSet.getString("country_name"));
+
+        resultSet.next();
+        resultSet.next();
+        resultSet.next();
+        //I'm in third row now
+        //I need to print all country names from first row to the last row
+
+
     }
 
 
